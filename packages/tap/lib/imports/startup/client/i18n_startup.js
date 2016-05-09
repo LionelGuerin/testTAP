2d54d854f34736235cd2dbd1720bab90170321da
Lionel Guerin <lionel@eliga.fr>
2016-05-09 14:01:06 +0200

    version package - work KO

--- packages/tap/lib/imports/startup/client/i18n_startup.js
+++ packages/tap/lib/imports/startup/client/i18n_startup.js
@@ -6,12 +6,13 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 Meteor.startup(function() {
     //Session.set("i18n", false);
     //
-    TAPi18n.setLanguage('es-MX')
-        .done(function () {
+    // TAPi18n.setLanguage('es-MX')
+    TAPi18n.setLanguage('en')
+        .done(function() {
             //Session.set("showLoadingIndicator", false);
             //console.log('hecho!!!');
         })
-        .fail(function (error_message) {
+        .fail(function(error_message) {
             // Handle the situation
             console.log(error_message);
         });
---
+++ packages/tap/package.js
@@ -0,0 +1,48 @@
+Package.describe({
+    name: "pack:tap",
+    version: "0.0.1",
+    summary: "Generic utilities",
+    git: "",
+    documentation: "README.md"
+});
+
+
+function configurePackage(api) {
+    api.versionsFrom("METEOR@1.3.2.4");
+
+    var impliedPackages = [
+        "standard-app-packages",
+        "tap:i18n@1.8.0",
+        "ecmascript"
+    ];
+
+    api.use(impliedPackages);
+    api.imply(impliedPackages);
+
+    api.addFiles([
+        "lib/imports/startup/client/i18n_startup.js",
+        "lib/i18n/en.i18n.json",
+        "lib/i18n/es-MX.i18n.json"]);
+
+    api.addFiles("server/main.js", ["server"]);
+
+    api.addFiles([
+        "client/main.html",
+        "client/main.css",
+        "client/main.js"], ["client"]);
+
+}
+
+
+
+Package.onUse(function(api) {
+    configurePackage(api);
+});
+
+Package.onTest(function(api) {
+    configurePackage(api);
+    api.use("tinytest");
+    api.use(["practicalmeteor:mocha@2.1.0_6"]);
+
+    api.addFiles("server/main.js", ["server"]);
+});
